3384e03558cc24cc7d88ea53c676065a0eb99cc6
danielvitorlopesia <127774149+danielvitorlopesia@users.noreply.github.com>
2026-03-11 21:21:17 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import theme from "./theme";
 import Home from "./pages/Home";
+import SpreadsheetDetail from "./pages/SpreadsheetDetail";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/spreadsheet/:id" element={<SpreadsheetDetail />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
