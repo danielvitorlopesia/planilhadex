06bf9eb3d061379b0e4388d9a9d57934072f989c
danielvitorlopesia <127774149+danielvitorlopesia@users.noreply.github.com>
2026-03-11 22:39:56 -0300

    Update [id].js

--- api/spreadsheets/[id].js
+++ api/spreadsheets/[id].js
@@ -1,1 +1,21 @@
+import { spreadsheets } from "../_data.js";
 
+export default function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({
+      message: "Método não permitido.",
+    });
+  }
+
+  const { id } = req.query;
+
+  const spreadsheet = spreadsheets.find((item) => item.id === String(id));
+
+  if (!spreadsheet) {
+    return res.status(404).json({
+      message: "Planilha não encontrada.",
+    });
+  }
+
+  return res.status(200).json(spreadsheet);
+}
