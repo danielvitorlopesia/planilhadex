42d9ab2443f083bece5bb3b1333c95a5f7e47ac8
danielvitorlopesia <127774149+danielvitorlopesia@users.noreply.github.com>
2026-03-11 21:05:08 -0300

    Simplify App component by removing routing
    Removed routing setup and directly rendered the Home component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import theme from "./theme";
 import Home from "./pages/Home";
-// Se você já tiver a página de detalhe criada, descomente a linha abaixo:
-// import SpreadsheetDetail from "./pages/SpreadsheetDetail";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/*
-            Se já existir a página de detalhe, use esta rota:
-            <Route path="/spreadsheet/:id" element={<SpreadsheetDetail />} />
-          */}
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </ThemeProvider>
   );
 }
