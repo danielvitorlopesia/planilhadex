5d540ff713f6c2f6466eee58f818d488ed08a130
danielvitorlopesia <127774149+danielvitorlopesia@users.noreply.github.com>
2026-03-11 23:07:28 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
 import React from "react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import theme from "./theme";
 import Home from "./pages/Home";
 import SpreadsheetDetail from "./pages/SpreadsheetDetail";
+import Login from "./pages/Login";
+import { AuthProvider } from "./auth/AuthContext";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/spreadsheet/:id" element={<SpreadsheetDetail />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/spreadsheet/:id"
+              element={
+                <ProtectedRoute>
+                  <SpreadsheetDetail />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
