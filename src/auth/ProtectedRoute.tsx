1fcfe47c4f93f31d0f26843ab0fc7d896a665fce
danielvitorlopesia <127774149+danielvitorlopesia@users.noreply.github.com>
2026-03-11 23:05:51 -0300

    Create ProtectedRoute.tsx

---
+++ src/auth/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "./AuthContext";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isAuthenticated } = useAuth();
+  const location = useLocation();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+
+  return <>{children}</>;
+}
