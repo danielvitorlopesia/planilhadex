cca81e853989bc711a778c9156adaa6af923e519
danielvitorlopesia <127774149+danielvitorlopesia@users.noreply.github.com>
2026-03-14 17:45:35 -0300

    Update SpreadsheetEditor.tsx

--- src/modules/spreadsheet-editor/SpreadsheetEditor.tsx
+++ src/modules/spreadsheet-editor/SpreadsheetEditor.tsx
@@ -7,21 +7,45 @@ import { SpreadsheetRecord } from "../../services/spreadsheetService";
 
 type Props = {
   spreadsheet: SpreadsheetRecord;
+  onSpreadsheetUpdated?: (spreadsheet: SpreadsheetRecord) => void;
 };
 
-export default function SpreadsheetEditor({ spreadsheet }: Props) {
+export default function SpreadsheetEditor({
+  spreadsheet,
+  onSpreadsheetUpdated,
+}: Props) {
   switch (spreadsheet.modelType) {
     case "dedicated_labor":
-      return <DedicatedLaborEditor spreadsheet={spreadsheet} />;
+      return (
+        <DedicatedLaborEditor
+          spreadsheet={spreadsheet}
+          onSpreadsheetUpdated={onSpreadsheetUpdated}
+        />
+      );
 
     case "non_dedicated_labor":
-      return <NonDedicatedLaborEditor spreadsheet={spreadsheet} />;
+      return (
+        <NonDedicatedLaborEditor
+          spreadsheet={spreadsheet}
+          onSpreadsheetUpdated={onSpreadsheetUpdated}
+        />
+      );
 
     case "service_composition":
-      return <ServiceCompositionEditor spreadsheet={spreadsheet} />;
+      return (
+        <ServiceCompositionEditor
+          spreadsheet={spreadsheet}
+          onSpreadsheetUpdated={onSpreadsheetUpdated}
+        />
+      );
 
     case "economic_rebalance":
-      return <EconomicRebalanceEditor spreadsheet={spreadsheet} />;
+      return (
+        <EconomicRebalanceEditor
+          spreadsheet={spreadsheet}
+          onSpreadsheetUpdated={onSpreadsheetUpdated}
+        />
+      );
 
     default:
       return <div>Modelo de planilha não reconhecido.</div>;
