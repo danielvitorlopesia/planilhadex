92980966ab34ecf3645c05a2aa733414cedd48db
danielvitorlopesia <127774149+danielvitorlopesia@users.noreply.github.com>
2026-03-15 12:59:26 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 
 import theme from "./theme";
 
@@ -12,60 +12,75 @@ import SpreadsheetCreatePage from "./pages/SpreadsheetCreatePage";
 
 import { AuthProvider } from "./auth/AuthContext";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import AppShell from "./components/AppShell";
+
+function AppRoutes() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+
+  if (isLoginPage) {
+    return (
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    );
+  }
+
+  return (
+    <AppShell>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/spreadsheet/:id"
+          element={
+            <ProtectedRoute>
+              <SpreadsheetDetail />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/models/new"
+          element={
+            <ProtectedRoute>
+              <ModelSelectorPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/models/new/create"
+          element={
+            <ProtectedRoute>
+              <SpreadsheetCreatePage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AppShell>
+  );
+}
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-
       <AuthProvider>
         <BrowserRouter>
-          <Routes>
-
-            <Route path="/login" element={<Login />} />
-
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/spreadsheet/:id"
-              element={
-                <ProtectedRoute>
-                  <SpreadsheetDetail />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/models/new"
-              element={
-                <ProtectedRoute>
-                  <ModelSelectorPage />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/models/new/create"
-              element={
-                <ProtectedRoute>
-                  <SpreadsheetCreatePage />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route path="*" element={<Navigate to="/" replace />} />
-
-          </Routes>
+          <AppRoutes />
         </BrowserRouter>
       </AuthProvider>
-
     </ThemeProvider>
   );
 }
